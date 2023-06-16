ab63a33798264d3c62a0dee96ca778cf1e0db003
catherinejj <catherine-jules@outlook.com>
2023-06-16 15:47:05 +0200

    correction du javascript

---
+++ assets/js/nav-menu.js
@@ -0,0 +1,11 @@
+
+let iiii = document.getElementById('logoDiscord');
+let nav = document.getElementsByClassName('liNav');
+iiii.addEventListener("mouseover", function(){
+            if(nav.style.display=="block"){
+                nav.style.display="none";
+            }
+            else{
+                nav.style.display="block";
+            }
+            });
--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,32 +1,27 @@
 </main>
     <!-- Debut des balises script-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        
-    <script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.bundle.js.map"></script>
-    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.bundle.min.js.map"></script>
-    <script src="assets/bootstrap/js/bootstrap.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.js.map"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js.map"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+   
+
+
+   
 <footer>
     <nav id="navFooter">
+
         <ul>
-            <li><a title=""><img alt="logo" src="assets/img/logo_instagram.png"></a></li>
-            <li><a title=""><img alt="logo" src="assets/img/logo_facebook.png"></a></li>
-            <li><a title=""><img alt="logo" src="assets/img/logo_tick_tock.png"></a></li>
-            <li><a title=""><img id="logoDiscord" class="sizeLogo" alt="logo discord" src="assets/img/logo_discord_rose.png"></a></li>
-            <li><a title=""><img alt="logo" src="assets/img/logo_twitter.png"></a></li>
-            <li><a title=""><img alt="logo" src="assets/img/logo_twitsh.png"></a></li>
-            <li><a title=""><img alt="logo" src="assets/img/logo_youtube_2.png"></a></li>
+            <li class="liNav"><a target="_blank" title="instagram" href="https://www.instagram.com/starisland.fr/"><img alt="logo" src="assets/img/logo_instagram.png"></a></li>
+            <li class="liNav"><a target="_blank" title="facebook" href="https://www.facebook.com/StarIslandfr-108004258577047"><img alt="logo" src="assets/img/logo_facebook.png"></a></li>
+            <li class="liNav"><a target="_blank" title="tiktok" href="https://www.tiktok.com/@star.island?lang=fr"><img alt="logo" src="assets/img/logo_tick_tock.png"></a></li>
+            <a target="_blank" title="discord" href="https://discord.gg/starisland" ><img id="logoDiscord" class="sizeLogo" alt="logo discord" src="assets/img/logo_discord_rose.png"></a>
+            <li class="liNav"><a target="_blank" title="twitter" href="https://twitter.com/StarIslandfr"><img alt="logo" src="assets/img/logo_twitter.png"></a></li>
+            <li class="liNav"><a target="_blank" title="twitch" href="https://www.twitch.tv/"><img alt="logo" src="assets/img/logo_twitsh.png"></a></li>
+            <li class="liNav"><a target="_blank" title="youtube" href="https://www.youtube.com/channel/UCI7G6fNN-17g1_tOVMKRCpQ"><img alt="logo" src="assets/img/logo_youtube_2.png"></a></li>
         </ul>
     </nav>
 </footer>
+  <script src="assets/jquery/jquery.min.js"></script>
+ <script src="assets/bootstrap/js/bootstrap.js"></script>
+ <script src="assets/js/nav-menu.js"></script>
 </body>
 </html>
 

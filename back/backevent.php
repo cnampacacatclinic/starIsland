db40ecb12b383ff50520ef8ade0e5bcf0c8d9e27
Catherine Jules <catherine-jules@outlook.com>
2023-07-01 11:24:18 +0200

    ajout texte dans formulaire pour la partie envent du backoffice

--- back/backevent.php
+++ back/backevent.php
@@ -74,14 +74,26 @@ require_once '../inc/backheader.inc.php';
 <form action="" method="post" class="w-75 mx-auto mt-5 mb-5">
         <div class="form-group">
             <small class="text-danger">*</small>
-            <label for="start date" class="form-label">Date de début</label>
+            <label for="start date" class="form-label"><?php
+            if(isset($_GET['id'])){
+                echo $event['start_date_event'];
+            }
+            else{
+                echo 'Date de debut';
+            }
+            ?></label>
             <!--<input min="<? //echo date('Y-m-d H:i:s');?>" name="start_date" id="start_date" type="datetime-local"
                    value="<? //echo $event['start_date_event'] ?? ''; ?>" class="form-control">-->
             <input min="<?=date('Y-m-d');?>" name="start_date" id="start_date" type="date"
                    value="<?=$event['start_date_event'] ?? ''; ?>" class="form-control">
             <small class="text-danger"><?= $error ?? ''; ?></small><br>
             <small class="text-danger">*</small>
-            <label for="end date" class="form-label">Date de fin</label>
+            <label for="end date" class="form-label"><?php
+            if(isset($_GET['id'])){
+                echo $event['end_date_event'];
+            }else{
+                echo 'Date de fin';}
+            ?></label>
             <!--<input min="<? //echo date('Y-m-d H:i:s');?>" name="end_date" id="en_date" placeholder="Date de fin" type="datetime-local" value="<? //echo $event['end_date_event'] ?? ''; ?>" class="form-control">-->
             <input min="<?=date('Y-m-d');?>" name="end_date" id="en_date" placeholder="Date de fin" type="date" value="<?=$event['end_date_event'] ?? ''; ?>" class="form-control">
             <small class="text-danger"><?= $error ?? ''; ?></small>
